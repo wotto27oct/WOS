3ec5c44771be4fdae6f8cb46b8effc1d04f5b1b3
wotto27oct <wotto27oct@gmail.com>
2019-03-23 03:57:50 +0900

    set color palette by making c-func in aseembly

--- harib00a/bootpack.c
+++ harib00a/bootpack.c
@@ -1,17 +1,67 @@
 void io_hlt(void);
-void write_mem8(int addr, int data); // assign 1-byte data to [addr]
+void io_cli(void);
+void io_cut8(int port, int data);
+int io_load_eflags(void);
+void io_store_eflags(int eflags);
+
+void init_palette(void);
+void set_palette(int start, int end, unsigned char *rgb);
 
 void HariMain(void)
 {
 	int i;
 	char *p;
 
-	for (i = 0xa0000; i <= 0xaffff; i++) {
-		p = (char *)i;
-		*p = i & 0x0f;
+	init_palette();	// initialize palette
+
+	p = (char *)0xa0000;
+
+	for (i = 0; i <= 0xffff; i++) {
+		p[i] = i & 0x0f;
 	}
 
 	for(;;) {
 		io_hlt();
 	}
+}
+
+void init_palette(void)
+{
+	static unsigned char table_rgb[16 * 3] = {
+		0x00, 0x00, 0x00,	// 0: black
+		0xff, 0x00, 0x00,	// 1: vivid red
+		0x00, 0xff, 0x00,	// 2: vivid green
+		0xff, 0xff, 0x00,	// 3: vivid yellow
+		0x00, 0x00, 0xff,	// 4: vivid blue
+		0xff, 0x00, 0xff,	// 5: vivid purple
+		0x00, 0xff, 0xff,	// 6: vivid sky
+		0xff, 0xff, 0xff,	// 7: white
+		0xc6, 0xc6, 0xc6,	// 8: vivid gray
+		0x84, 0x00, 0x00,	// 9: dark red
+		0x00, 0x84, 0x00,	// 10: dark green
+		0x84, 0x84, 0x00,	// 11: dark yellow
+		0x00, 0x00, 0x84,	// 12: dark blue
+		0x84, 0x00, 0x84,	// 13: dark purple
+		0x00, 0x84, 0x84,	// 14: dark sky
+		0x84, 0x84, 0x84	// 15: dark grey
+	};
+	set_palette(0, 15, table_rgb);
+	return;
+
+}
+
+void set_palette(int start, int end, unsigned char *rgb)
+{
+	int i, eflags;
+	eflags = io_load_eflags();
+	io_cli();
+	io_out8(0x03c8, start);
+	for (i = start; i <= end; i++) {
+		io_out8(0x03c9, rgb[0] / 4);
+		io_out8(0x03c9, rgb[1] / 4);
+		io_out8(0x03c9, rgb[2] / 4);
+		rgb += 3;
+	}
+	io_store_eflags(eflags);
+	return;
 }
--- harib00a/naskfunc.nas
+++ harib00a/naskfunc.nas
@@ -7,8 +7,10 @@
 ; information for object file
 [FILE "naskfunc.nas"]	; source file name info
 
-		GLOBAL	_io_hlt,_write_mem8
-; function entity
+		GLOBAL	_io_hlt, _io_cli, _io_sti, _io_stihlt
+		GLOBAL	_io_in8,  _io_in16,  _io_in32
+		GLOBAL	_io_out8, _io_out16, _oi_out32
+		GLOBAL	_io_load_eflags, _io_store_eflags
 
 [SECTION .text]
 
@@ -16,8 +18,61 @@ _io_hlt:	; void io_hlt(void);
 		HLT
 		RET
 
-_write_mem8:	; void write_mem8(int addr, int data);
-		MOV		ECX,[ESP+4]		; addr is in [ESP+4]
-		MOV		AL,[ESP+8]		; because ESP is stack pointer.
-		MOV		[ECX],AL
+_io_cli:
+		CLI
+		RET
+
+_io_sti:
+		STI
+		RET
+
+_io_stihlt:
+		STI
+		HLT
+		RET
+
+_io_in8:		;int io_in8(int port);
+		MOV		EDX,[ESP+4]
+		MOV		EAX,0
+		IN		AL,DX
+		RET
+
+_io_in16:
+		MOV		EDX,[ESP+4]
+		MOV		EAX,0
+		IN		AX,DX
+		RET
+
+_io_in32:
+		MOV		EDX,[ESP+4]
+		IN		EAX,DX
+		RET
+
+_io_out8:		; void io_out8(int port, int data);
+		MOV		EDX,[ESP+4]
+		MOV		AL,[ESP+8];
+		OUT		DX,AL
+		RET
+
+_io_out16:		; void io_out16(int port, int data);
+		MOV		EDX,[ESP+4]
+		MOV		EAX,[ESP+8];
+		OUT		DX,AX
+		RET
+
+_io_out32:		; void io_out32(int port, int data);
+		MOV		EDX,[ESP+4]
+		MOV		EAX,[ESP+8];
+		OUT		DX,EAX
+		RET
+
+_io_load_eflags:
+		PUSHFD		; = PUSH EFLAGS
+		POP		EAX
+		RET
+
+_io_store_eflags:
+		MOV		EAX,[ESP+4]
+		PUSH	EAX
+		POPFD		; = POP EFLAGS
 		RET
