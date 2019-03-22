b51cb47154578ed374557811e3881c803b36269c
wotto27oct <wotto27oct@gmail.com>
2019-03-23 03:07:26 +0900

    make striped screen by using pointer in c

--- harib00a/bootpack.c
+++ harib00a/bootpack.c
@@ -4,9 +4,11 @@ void write_mem8(int addr, int data); // assign 1-byte data to [addr]
 void HariMain(void)
 {
 	int i;
+	char *p;
 
 	for (i = 0xa0000; i <= 0xaffff; i++) {
-		write_mem8(i, i & 0x0f);	// MOV BYTE [i],15
+		p = (char *)i;
+		*p = i & 0x0f;
 	}
 
 	for(;;) {
