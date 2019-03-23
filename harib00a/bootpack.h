bed2c4c8b04472daf38440c3537012ea1a2e4f25
wotto27oct <wotto27oct@gmail.com>
2019-03-24 01:07:18 +0900

    divide files and fix Makefile

---
+++ harib00a/bootpack.h
@@ -0,0 +1,71 @@
+// asmhead.nas
+struct BOOTINFO {
+	char cyls, leds, vmode, reserve;
+	short scrnx, scrny;
+	char *vram;
+};
+#define ADR_BOOTINFO	0x00000ff0
+
+// naskfunc.nas
+void io_hlt(void);
+void io_cli(void);
+void io_out8(int port, int data);
+int io_load_eflags(void);
+void io_store_eflags(int eflags);
+void load_gdtr(int limit, int addr);
+void load_idtr(int limit, int addr);
+
+// graphic.c
+void init_palette(void);
+void set_palette(int start, int end, unsigned char *rgb);
+void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
+void init_screen8(char *vram, int x, int y);
+void putfont8(char*, int, int, int, char, char*);
+void putfont8_sec(char*, int, int, int, char, unsigned char*);
+void init_mouse_cursor8(char *mouse, char bc);
+void putblock8_8(char *vram, int vxsize, int pxsize,
+		int pysize, int px0, int py0, char *buf, int bxsize);
+
+#define COL8_BLACK		0
+#define COL8_VIVRED		1
+#define COL8_VIVGREEN		2
+#define COL8_VIVYELLOW		3
+#define COL8_VIVBLUE		4
+#define COL8_VIVPURPLE		5
+#define COL8_VIVSKY		6
+#define COL8_WHITE		7
+#define COL8_VIVGRAY		8
+#define COL8_DARKRED		9
+#define COL8_DARKGREEN		10
+#define COL8_DARKYELLOW		11
+#define COL8_DARKBLUE		12
+#define COL8_DARKPURPLE	13
+#define COL8_DARKSKY		14
+#define COL8_DARKGRAY		15
+
+// dsctbl.c
+struct SEGMENT_DESCRIPTOR {
+	short limit_low, base_low;
+	char base_mid, access_right;
+	char limit_high, base_high;
+};
+
+struct GATE_DESCRIPTOR {
+	short offset_low, selector;
+	char dw_count, access_right;
+	short offset_high;
+};
+
+void init_gdtidt(void);
+void set_segmdesc(struct SEGMENT_DESCRIPTOR *sd, unsigned int limit, int base, int ar);
+void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar);
+
+#define ADR_IDT			0x0026f800
+#define LIMIT_IDT		0x000007ff
+#define ADR_GDT			0x00270000
+#define LIMIT_GDT		0x0000ffff
+#define ADR_BOTPAK		0x00280000
+#define LIMIT_BOTPAK	0x0007ffff
+#define AR_DATA32_RW	0x4092
+#define AR_CODE32_ER	0x409a
+
---
+++ harib00a/graphic.c
@@ -0,0 +1,153 @@
+#include "bootpack.h"
+
+void init_palette(void)
+{
+	static unsigned char table_rgb[16 * 3] = {
+		0x00, 0x00, 0x00,	// 0: black
+		0xff, 0x00, 0x00,	// 1: vivid red
+		0x00, 0xff, 0x00,	// 2: vivid green
+		0xff, 0xff, 0x00,	// 3: vivid yellow
+		0x00, 0x00, 0xff,	// 4: vivid blue
+		0xff, 0x00, 0xff,	// 5: vivid purple
+		0x00, 0xff, 0xff,	// 6: vivid sky
+		0xff, 0xff, 0xff,	// 7: white
+		0xc6, 0xc6, 0xc6,	// 8: vivid gray
+		0x84, 0x00, 0x00,	// 9: dark red
+		0x00, 0x84, 0x00,	// 10: dark green
+		0x84, 0x84, 0x00,	// 11: dark yellow
+		0x00, 0x00, 0x84,	// 12: dark blue
+		0x84, 0x00, 0x84,	// 13: dark purple
+		0x00, 0x84, 0x84,	// 14: dark sky
+		0x84, 0x84, 0x84	// 15: dark gray
+	};
+	set_palette(0, 15, table_rgb);
+	return;
+
+}
+
+void set_palette(int start, int end, unsigned char *rgb)
+{
+	int i, eflags;
+	eflags = io_load_eflags();
+	io_cli();
+	io_out8(0x03c8, start);
+	for (i = start; i <= end; i++) {
+		io_out8(0x03c9, rgb[0] / 4);
+		io_out8(0x03c9, rgb[1] / 4);
+		io_out8(0x03c9, rgb[2] / 4);
+		rgb += 3;
+	}
+	io_store_eflags(eflags);
+	return;
+}
+
+void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1)
+{
+	int x, y;
+	for (y = y0; y <= y1; y++) {
+		for (x = x0; x <= x1; x++) {
+			vram[y * xsize + x] = c;
+		}
+	}
+	return;
+}
+
+void init_screen8(char *vram, int xsize, int ysize)
+{
+	boxfill8(vram, xsize, COL8_DARKSKY,  0,         0,          xsize -  1, ysize - 29);
+	boxfill8(vram, xsize, COL8_VIVGRAY,  0,         ysize - 28, xsize -  1, ysize - 28);
+	boxfill8(vram, xsize, COL8_WHITE,  0,         ysize - 27, xsize -  1, ysize - 27);
+	boxfill8(vram, xsize, COL8_VIVGRAY,  0,         ysize - 26, xsize -  1, ysize -  1);
+
+	boxfill8(vram, xsize, COL8_WHITE,  3,         ysize - 24, 59,         ysize - 24);
+	boxfill8(vram, xsize, COL8_WHITE,  2,         ysize - 24,  2,         ysize -  4);
+	boxfill8(vram, xsize, COL8_DARKGRAY,  3,         ysize -  4, 59,         ysize -  4);
+	boxfill8(vram, xsize, COL8_DARKGRAY, 59,         ysize - 23, 59,         ysize -  5);
+	boxfill8(vram, xsize, COL8_BLACK,  2,         ysize -  3, 59,         ysize -  3);
+	boxfill8(vram, xsize, COL8_BLACK, 60,         ysize - 24, 60,         ysize -  3);
+
+	boxfill8(vram, xsize, COL8_DARKGRAY, xsize - 47, ysize - 24, xsize -  4, ysize - 24);
+	boxfill8(vram, xsize, COL8_DARKGRAY, xsize - 47, ysize - 23, xsize - 47, ysize -  4);
+	boxfill8(vram, xsize, COL8_WHITE, xsize - 47, ysize -  3, xsize -  4, ysize -  3);
+	boxfill8(vram, xsize, COL8_WHITE, xsize -  3, ysize - 24, xsize -  3, ysize -  3);
+}
+
+void putfont8(char *vram, int xsize, int x, int y, char c, char *font)
+{
+	int i;
+	char *p, d;
+	for (i = 0; i < 16; i++) {
+		p = vram + (y + i) * xsize + x;
+		d = font[i];
+		if ((d & 0x80) != 0) p[0] = c;
+		if ((d & 0x40) != 0) p[1] = c;
+		if ((d & 0x20) != 0) p[2] = c;
+		if ((d & 0x10) != 0) p[3] = c;
+		if ((d & 0x08) != 0) p[4] = c;
+		if ((d & 0x04) != 0) p[5] = c;
+		if ((d & 0x02) != 0) p[6] = c;
+		if ((d & 0x01) != 0) p[7] = c;
+	}
+	return;
+}
+
+void putfont8_sec(char *vram, int xsize, int x, int y, char c, unsigned char *s)
+{
+	extern char hankaku[4096];
+	for (; *s != 0x00; s++) {
+		putfont8(vram, xsize, x, y, c, hankaku + *s * 16);
+		x += 8;
+	}
+	return;
+}
+
+void init_mouse_cursor8(char *mouse, char bc)
+{
+	static char cursor[16][16] = {
+		"**************..",
+		"*OOOOOOOOOOO*...",
+		"*OOOOOOOOOO*....",
+		"*OOOOOOOOO*.....",
+		"*OOOOOOOO*......",
+		"*OOOOOOO*.......",
+		"*OOOOOOO*.......",
+		"*OOOOOOOO*......",
+		"*OOOO**OOO*.....",
+		"*OOO*..*OOO*....",
+		"*OO*....*OOO*...",
+		"*O*......*OOO*..",
+		"**........*OOO*.",
+		"*..........*OOO*",
+		"............*OO*",
+		".............***"
+	};
+	int x, y;
+
+	for (y = 0; y < 16; y++) {
+		for (x = 0; x < 16; x++) {
+			if (cursor[y][x] == '*') {
+				mouse[y * 16 + x] = COL8_BLACK;
+			}
+			if (cursor[y][x] == 'O') {
+				mouse[y * 16 + x] = COL8_WHITE;
+			}
+			if (cursor[y][x] == '.') {
+				mouse[y * 16 + x] = bc;
+			}
+		}
+	}
+	return;
+}
+
+
+void putblock8_8(char *vram, int vxsize, int pxsize,
+		int pysize, int px0, int py0, char *buf, int bxsize)
+{
+	int x, y;
+	for (y = 0; y < pysize; y++) {
+		for (x = 0; x < pxsize; x++) {
+			vram[(py0+y) * vxsize + (px0 + x)] = buf[y * bxsize + x];
+		}
+	}
+	return;
+}
