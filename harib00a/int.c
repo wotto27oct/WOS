22637df1621f0ef0d1e2f0c0116b086d7ef2ad22
wotto27oct <wotto27oct@gmail.com>
2019-03-24 03:12:18 +0900

    improve key-buffa

--- harib00a/int.c
+++ harib00a/int.c
@@ -31,9 +31,11 @@ void inthandler21(int *esp)
 	io_out8(PIC0_OCW2, 0x61);	// report PIC that IRQ-01 is successfully accepted
 	data = io_in8(PORT_KEYDAT);
 
-	if (keybuf.next < 32) {
-		keybuf.data[keybuf.next] = data;
-		keybuf.next++;
+	if (keybuf.len < 32) {
+		keybuf.data[keybuf.next_w] = data;
+		keybuf.len++;
+		keybuf.next_w++;
+		if (keybuf.next_w == 32) keybuf.next_w = 0;
 	}
 	return;
 }
