b19fefc2ebcf81216bdf7fcc3281e9caa7a14bf4
wotto27oct <wotto27oct@gmail.com>
2019-03-24 22:25:23 +0900

    improve smoothness

--- harib00a/sheet.c
+++ harib00a/sheet.c
@@ -73,7 +73,7 @@ void sheet_updown(struct SHTCTL *ctl, struct SHEET *sht, int height)
 			}
 			ctl->top--;
 		}
-		sheet_refresh(ctl);
+		sheet_refreshsub(ctl, sht->vx0, sht->vy0, sht->vx0 + sht->bxsize, sht->vy0 + sht->bysize);
 	} else if (old < height) {
 		// up
 		if (old >= 0) {
@@ -91,12 +91,20 @@ void sheet_updown(struct SHTCTL *ctl, struct SHEET *sht, int height)
 			ctl->sheets[height] = sht;
 			ctl->top++;
 		}
-		sheet_refresh(ctl);
+		sheet_refreshsub(ctl, sht->vx0, sht->vy0, sht->vx0 + sht->bxsize, sht->vy0 + sht->bysize);
 	}
 	return;
 }
 
-void sheet_refresh(struct SHTCTL *ctl)
+void sheet_refresh(struct SHTCTL *ctl, struct SHEET *sht, int bx0, int by0, int bx1, int by1)
+{
+	if (sht->height >= 0) {
+		sheet_refreshsub(ctl, sht->vx0 + bx0, sht->vy0 + by0, sht->vx0 + bx1, sht->vy0 + by1);
+	}
+	return;
+}
+
+void sheet_refreshsub(struct SHTCTL *ctl, int vx0, int vy0, int vx1, int vy1)
 {
 	int h, bx, by, vx, vy;
 	unsigned char *buf, c, *vram = ctl->vram;
@@ -108,23 +116,26 @@ void sheet_refresh(struct SHTCTL *ctl)
 			vy = sht->vy0 + by;
 			for (bx = 0; bx < sht->bxsize; bx++) {
 				vx = sht->vx0 + bx;
-				c = buf[by * sht->bxsize + bx];
-				if (c != sht->col_inv) {
-					vram[vy * ctl->xsize + vx] = c;
+				if (vx0 <= vx && vx < vx1 && vy0 <= vy && vy < vy1) {
+					c = buf[by * sht->bxsize + bx];
+					if (c != sht->col_inv) {
+						vram[vy * ctl->xsize + vx] = c;
+					}
 				}
 			}
 		}
 	}
 	return;
 }
-
 void sheet_slide(struct SHTCTL *ctl, struct SHEET *sht, int vx0, int vy0)
 {
+	int old_vx0 = sht->vx0, old_vy0 = sht->vy0;
 	sht->vx0 = vx0;
 	sht->vy0 = vy0;
 	if (sht->height >= 0) {
 		// if it's visible, refresh screen
-		sheet_refresh(ctl);
+		sheet_refreshsub(ctl, old_vx0, old_vy0, old_vx0 + sht->bxsize, old_vy0 + sht->bysize);
+		sheet_refreshsub(ctl, vx0, vy0, vx0 + sht->bxsize, vy0 + sht->bysize);
 	}
 	return;
 }
