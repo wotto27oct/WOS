432f20848657c0ab39c1e092f8b44bb868887533
wotto27oct <wotto27oct@gmail.com>
2019-03-24 20:49:26 +0900

    implement memory management system

--- harib00a/bootpack.c
+++ harib00a/bootpack.c
@@ -3,8 +3,26 @@
 extern struct FIFO8 keyfifo;
 extern struct FIFO8 mousefifo;
 
+#define MEMMAN_FREES		4090	// about 32KB
+#define MEMMAN_ADDR			0x003c0000
+
+struct FREEINFO {
+	unsigned int addr, size;
+};
+
+struct MEMMAN {
+	int frees, maxfrees, lostsize, losts;
+	struct FREEINFO free[MEMMAN_FREES];
+};
+
+
 unsigned int memtest(unsigned int start, unsigned int end);
 unsigned int memtest_sub(unsigned int start, unsigned int end);
+
+void memman_init(struct MEMMAN *man);
+unsigned int memman_total(struct MEMMAN *man);
+unsigned int memman_alloc(struct MEMMAN *man, unsigned int size);
+int memman_free(struct MEMMAN *man, unsigned int addr, unsigned int size);
 
 void HariMain(void)
 {
@@ -12,6 +30,8 @@ void HariMain(void)
 	char s[40], mcursor[256], keybuf[32], mousebuf[128];
 	int mx, my, i;
 	struct MOUSE_DEC mdec;
+	unsigned int memtotal;
+	struct MEMMAN *memman = (struct MEMMAN *) MEMMAN_ADDR;
 
 	init_gdtidt();
 	init_pic();
@@ -26,6 +46,11 @@ void HariMain(void)
 	init_keyboard();
 	enable_mouse(&mdec);
 
+	memtotal = memtest(0x00400000, 0xbfffffff);
+	memman_init(memman);
+	memman_free(memman, 0x00001000, 0x0009e000);
+	memman_free(memman, 0x00400000, memtotal - 0x00400000);
+
 	init_palette();
 	init_screen8(binfo->vram, binfo->scrnx, binfo->scrny);
 
@@ -36,10 +61,9 @@ void HariMain(void)
 	sprintf(s, "(%d, %d)", mx, my);
 	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_WHITE, s);
 
-	i = memtest(0x00400000, 0xbfffffff) / (1024 * 1024);
-	sprintf(s, "memory %dMB", i);
+	sprintf(s, "memory %dMB   free : %dKB",
+			memtotal / (1024 * 1024), memman_total(memman) / 1024);
 	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 32, COL8_WHITE, s);
-
 
 	for(;;) {
 		io_cli();
@@ -132,3 +156,97 @@ unsigned int memtest(unsigned int start, unsigned int end) {
 	return i;
 }
 
+
+void memman_init(struct MEMMAN *man)
+{
+	man->frees = 0;
+	man->maxfrees = 0;
+	man->lostsize = 0;
+	man->losts = 0;
+	return;
+}
+
+unsigned int memman_total(struct MEMMAN *man)
+{
+	unsigned int i, t = 0;
+	for (i = 0; i < man->frees; i++) {
+		t += man->free[i].size;
+	}
+	return t;
+}
+
+unsigned int memman_alloc(struct MEMMAN *man, unsigned int size)
+{
+	unsigned int i, a;
+
+	for (i = 0; i < man->frees; i++) {
+		if (man->free[i].size >= size) {
+			a = man->free[i].addr;
+			man->free[i].addr += size;
+			man->free[i].size -= size;
+			if (man->free[i].size == 0) {
+				// we lost free[i]
+				man->frees--;
+				for (; i < man->frees; i++) {
+					man->free[i] = man->free[i + 1];
+				}
+			}
+			return a;
+		}
+	}
+	return 0; // no memory
+}
+
+int memman_free(struct MEMMAN *man, unsigned int addr, unsigned int size) {
+	int i, j;
+	// free[] must be sorted in address
+	for (i = 0; i < man->frees; i++) {
+		if (man->free[i].addr > addr) {
+			break;
+		} else if (i == man->frees - 1) {
+			i = man->frees;
+			break;
+		}
+	}
+
+	// free[i - 1].addr < addr < free[i].addr
+	if (i > 0) {
+		if (man->free[i - 1].addr + man->free[i - 1].size == addr) {
+			man->free[i - 1].size += size;
+			if (i < man->frees) {
+				if (addr + size == man->free[i].addr) {
+					man->free[i - 1].size += man->free[i].size;
+					man->frees--;
+					for (; i < man->frees; i++) {
+						man->free[i] = man->free[i + 1];
+					}
+				}
+			}
+			return 0;
+		}
+	}
+
+	if (i < man->frees) {
+		if (addr + size == man->free[i].addr) {
+			man->free[i].addr = addr;
+			man->free[i].size += size;
+			return 0;
+		}
+	}
+
+	if (man->frees < MEMMAN_FREES) {
+		for (j = man->frees; j > i; j--) {
+			man->free[j] = man->free[j - 1];
+		}
+		man->frees++;
+		if (man->maxfrees < man->frees) {
+			man->maxfrees = man->frees;
+		}
+		man->free[i].addr = addr;
+		man->free[i].size = size;
+		return 0;
+	}
+	man->losts++;
+	man->lostsize += size;
+	return -1;
+}
