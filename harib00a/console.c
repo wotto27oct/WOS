d96ed27ff9807f7d30c29d27e5b185e3216d854f
wotto27oct <wotto27oct@gmail.com>
2019-04-03 04:59:27 +0900

    execute application

--- harib00a/console.c
+++ harib00a/console.c
@@ -10,6 +10,7 @@ void console_task(struct SHEET *sheet, unsigned int memtotal)
 	char s[30], cmdline[30], *p;
 	struct MEMMAN *memman = (struct MEMMAN *) MEMMAN_ADDR;
 	struct FILEINFO *finfo = (struct FILEINFO *) (ADR_DISKIMG + 0x002600);
+	struct SEGMENT_DESCRIPTOR *gdt = (struct SEGMENT_DESCRIPTOR *) ADR_GDT;
 
 	int *fat = (int *) memman_alloc_4k(memman, 4 * 2880);
 
@@ -178,6 +179,45 @@ type_next_file:
 						cursor_y = cons_newline(cursor_y, sheet);
 						
 
+					} else if (strcmp(cmdline, "hlt") == 0) {
+						// execute hlt application
+						for (y = 0; y < 11; y++) {
+							s[y] = ' ';
+						}
+						s[0] = 'H';
+						s[1] = 'L';
+						s[2] = 'T';
+						s[8] = 'H';
+						s[9] = 'R';
+						s[10] = 'B';
+						for (x = 0; x < 224; ) {
+							if (finfo[x].name[0] == 0x00) {
+								break;
+							}
+							if ((finfo[x].type & 0x18) == 0) {
+								for (y = 0; y < 11; y++) {
+									if (finfo[x].name[y] != s[y]) {
+										goto hlt_next_file;
+									}
+								}
+								break;
+							}
+					hlt_next_file:
+							x++;
+						}
+						if (x < 224 && finfo[x].name[0] != 0x00) {
+							// file found
+							p = (char *) memman_alloc_4k(memman, finfo[x].size);
+							file_loadfile(finfo[x].clustno, finfo[x].size, p, fat, (char *) (ADR_DISKIMG + 0x003e00));
+							set_segmdesc(gdt + 1003, finfo[x].size - 1, (int) p, AR_CODE32_ER);
+							farjmp(0, 1003 * 8);
+							memman_free_4k(memman, (int) p, finfo[x].size);
+						} else {
+							// file not found
+							putfonts8_asc_sht(sheet, 8, cursor_y, COL8_WHITE, COL8_BLACK, "File not found", 15);
+							cursor_y = cons_newline(cursor_y, sheet);
+						}
+						cursor_y = cons_newline(cursor_y, sheet);
 					} else if (cmdline[0] != 0) {
 						// not command, but not empty
 						putfonts8_asc_sht(sheet, 8, cursor_y, COL8_WHITE, COL8_BLACK, "Bad Command.", 12);
