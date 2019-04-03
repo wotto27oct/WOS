ffd4dcfc25dcf07e3244e61b116efd7cfcb1e050
wotto27oct <wotto27oct@gmail.com>
2019-04-04 03:49:22 +0900

    implement app

--- harib00a/console.c
+++ harib00a/console.c
@@ -151,13 +151,13 @@ void cons_runcmd(char *cmdline, struct CONSOLE *cons, int *fat, unsigned int mem
 		cmd_dir(cons);
 	} else if (strncmp(cmdline, "type ", 5) == 0) {
 		cmd_type(cons, fat, cmdline);
-	} else if (strcmp(cmdline, "hlt") == 0) {
-		cmd_hlt(cons, fat);
 	} else if (cmdline[0] != 0) {
-		// not command, but not enpty line
-		putfonts8_asc_sht(cons->sht, 8, cons->cur_y, COL8_WHITE, COL8_BLACK, "Bad command.", 12);
-		cons_newline(cons);
-		cons_newline(cons);
+		if (cmd_app(cons, fat, cmdline) == 0) {
+			// not command, not app, but not empty line
+			putfonts8_asc_sht(cons->sht, 8, cons->cur_y, COL8_WHITE, COL8_BLACK, "Bad command.", 12);
+			cons_newline(cons);
+			cons_newline(cons);
+		}
 	}
 	return;
 }
@@ -236,11 +236,31 @@ void cmd_type(struct CONSOLE *cons, int *fat, char *cmdline) {
 	return;
 }
 
-void cmd_hlt(struct CONSOLE *cons, int *fat) {
+int cmd_app(struct CONSOLE *cons, int *fat, char *cmdline) {
 	struct MEMMAN *memman = (struct MEMMAN *) MEMMAN_ADDR;
-	struct FILEINFO *finfo = file_search("HLT.HRB", (struct FILEINFO *) (ADR_DISKIMG + 0x002600), 224);
+	struct FILEINFO *finfo;
 	struct SEGMENT_DESCRIPTOR *gdt = (struct SEGMENT_DESCRIPTOR *) ADR_GDT;
-	char *p;
+	char *p, name[18];
+	int i;
+
+	for (i = 0; i < 13; i++) {
+		if (cmdline[i] <= ' ') {
+			break;
+		}
+		name[i] = cmdline[i];
+	}
+	name[i] = 0;
+
+	finfo = file_search(name, (struct FILEINFO *) (ADR_DISKIMG + 0x002600), 224);
+	if (finfo == 0 && name[i - 1] != '.') {
+		// add .HRB
+		name[i] = '.';
+		name[i + 1] = 'H';
+		name[i + 2] = 'R';
+		name[i + 3] = 'B';
+		name[i + 4] = 0;
+		finfo = file_search(name, (struct FILEINFO *) (ADR_DISKIMG + 0x002600), 224);
+	}
 	if (finfo != 0) {
 		// file found
 		p = (char *) memman_alloc_4k(memman, finfo->size);
@@ -248,11 +268,8 @@ void cmd_hlt(struct CONSOLE *cons, int *fat) {
 		set_segmdesc(gdt + 1003, finfo->size - 1, (int) p, AR_CODE32_ER);
 		farcall(0, 1003 * 8);
 		memman_free_4k(memman, (int) p, finfo->size);
-	} else {
-		// file not found
-		putfonts8_asc_sht(cons->sht, 8, cons->cur_y, COL8_WHITE, COL8_BLACK, "File not found.", 15);
 		cons_newline(cons);
+		return 1;
 	}
-	cons_newline(cons);
-	return;
+	return 0;
 }
